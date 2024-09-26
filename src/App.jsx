abe5ece42d2c3c221b0d62212b87facf7de6ebbb
Omar <omarsabry425@gmail.com>
2024-09-26 22:33:31 +0300

    Fix error

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import ProductDetails from './components/productDetails/productDetails'
+import ProductDetails from './components/ProductDetails/ProductDetails'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import { UserTokenContext, UserTokenContextProvider } from './contexts/UserTokenContext/UserTokenContext'
